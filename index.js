b33738487024fc70d42591ea1015ddc568255b1f
ismaelsoares <ismaelsoaresdoria@gmail.com>
2023-04-12 11:33:57 -0300

    Inicialização do sistema e importação dos modulos do inquirer e chalk

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+//modulos externos
 const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+//modulos internos
+const fs = require('fs');
+
+(function operation(){
+
+    inquirer.prompt([
+        {
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: ['Criar Conta','Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
+        },
+    ]).then(answer => {
+        const action = answer['action']
+
+        console.log(action);
+    })
+    .catch(err => console.log(err));
+
+})();
